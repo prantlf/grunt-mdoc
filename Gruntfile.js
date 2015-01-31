461e1c8d5e627dda37a113162ce78458cd67d4fb
Ferdinand Prantl <prantlf@gmail.com>
2015-01-31 23:02:13 +0100

    Add sunarqube runner for the local testing server

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,31 @@ module.exports = function (grunt) {
       tests: {
         src: 'coverage/tests.lcov'
       }
+    },
+
+    sonarRunner: {
+      analysis: {
+        options: {
+          debug: true,
+          separator: '\n',
+          sonar: {
+            host: {
+              url: 'http://localhost:9000'
+            },
+            jdbc: {
+              url: 'jdbc:mysql://localhost:3306/sonar',
+              username: 'sonar',
+              password: 'sonar'
+            },
+            projectKey: 'sonar:grunt-sonar-runner:0.1.0',
+            projectName: 'grunt-mdoc',
+            projectVersion: '0.1.3',
+            sources: 'tasks',
+            language: 'js',
+            sourceEncoding: 'UTF-8'
+          }
+        }
+      }
     }
 
   });
@@ -67,6 +92,7 @@ module.exports = function (grunt) {
   grunt.registerTask('test', ['jshint', 'clean:tests', 'mdoc', 'nodeunit']);
   grunt.registerTask('instrument', ['jshint', 'clean', 'jscoverage']);
   grunt.registerTask('post_coverage', ['test', 'coveralls']);
+  grunt.registerTask('analyze', ['sonarRunner']);
   grunt.registerTask('default', ['test']);
 
 };
